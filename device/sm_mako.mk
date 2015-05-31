4cb57a7592ba45002302d0e87464f9256f83c7cb
Paul Beeler <pbeeler80@gmail.com>
2015-05-31 14:00:05 -0600

    Enable -O3 for mako [1/2]
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- device/sm_mako.mk
+++ device/sm_mako.mk
@@ -26,6 +26,7 @@ ifeq ($(strip $(HOST_OS)),linux)
   # Sabermod configs
   ENABLE_STRICT_ALIASING := true
 export ENABLE_PTHREAD := false
+export LOCAL_O3 := true
 
 GRAPHITE_KERNEL_FLAGS := \
   -fopenmp
