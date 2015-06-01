122ef77bac09a9face7bd34e74e6e49ac4f2b252
Paul Beeler <pbeeler80@gmail.com>
2015-06-01 13:31:55 -0600

    Add inline kernel stuff
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

---
+++ device/kernel.mk
@@ -0,0 +1,25 @@
+# Compile the kernel inline
+
+# Note for this to work you will need to remove prebuilt kernel building in device tree,
+# and any other inline kernel building implimentations.
+
+ifneq ($(filter %hammerhead,$(TARGET_PRODUCT)),)
+  KERNEL_DEFCONFIG := sabermod_hammerhead_defconfig
+  KERNEL_DIR := kernel/lge/hammerhead
+  KERNEL_BINARY_IMAGE := zImage-dtb
+endif
+
+ifneq ($(filter %mako,$(TARGET_PRODUCT)),)
+  KERNEL_DEFCONFIG := sabermod_mako_defconfig
+  KERNEL_DIR := kernel/lge/mako
+  KERNEL_BINARY_IMAGE := zImage
+endif
+
+ifdef KERNEL_DIR
+  include $(KERNEL_DIR)/AndroidKernel.mk
+endif
+
+# cp will do.
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
