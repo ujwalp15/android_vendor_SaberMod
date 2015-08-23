fc41de7ca2c21562b73c54a8e4a3f38bf959441f
Paul Beeler <pbeeler80@gmail.com>
2015-08-23 17:53:07 -0400

    Install arm64 libs to lib64
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- prebuilts/system/lib/arm64/supersu.mk
+++ prebuilts/system/lib/arm64/supersu.mk
@@ -1,3 +1,3 @@
 PRODUCT_COPY_FILES += \
-    $(SM_VENDOR)/prebuilts/system/lib/arm64/libsupol.so:system/lib/libsupol.so
+    $(SM_VENDOR)/prebuilts/system/lib/arm64/libsupol.so:system/lib64/libsupol.so
 
