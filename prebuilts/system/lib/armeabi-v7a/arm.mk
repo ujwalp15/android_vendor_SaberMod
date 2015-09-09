e13d208c44d72f6b20c8876a8c2dd12c36fdb347
infected_ <zzitoz@gmail.com>
2015-09-09 18:02:37 +0100

    Revert "[armv7-a] Add prebuilt mozilla firefox browser compiled with sm-4.9 by @frap129"
    This reverts commit 51c31802c02d4e7c9084c259fc08d85959664132.
    
    Conflicts:
    	device/sm_devices.mk
    	prebuilts/system/app/Android.mk
    	prebuilts/system/lib/armeabi-v7a/arm.mk
    
    Change-Id: Ib18bdb0ee903d1b1457c52e4bb6662ff6588ef28

--- prebuilts/system/lib/armeabi-v7a/arm.mk
+++
@@ -1,4 +0,0 @@
-PRODUCT_COPY_FILES += \
-    $(SM_VENDOR)/prebuilts/system/lib/armeabi-v7a/libmozglue.so:system/lib/libmozglue.so \
-    $(SM_VENDOR)/prebuilts/system/lib/armeabi-v7a/libplugin-container.so:system/lib/libplugin-container.so \
-    $(SM_VENDOR)/prebuilts/system/lib/armeabi-v7a/libplugin-container-pie.so:system/lib/libplugin-container-pie.so
