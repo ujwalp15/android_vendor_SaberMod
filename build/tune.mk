91ba72d6bd2e50916c42d543235722c5f4d7a7cf
Paul Beeler <pbeeler80@gmail.com>
2015-09-14 18:18:09 -0400

    Update tune.mk
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- build/tune.mk
+++ build/tune.mk
@@ -18,60 +18,75 @@ CORTEX_A15_TYPE := \
 CORTEX_A57_TYPE := \
 	cortex-a57 \
 	denver
+
+ifneq (true,$(strip $(ENABLE_GCC_DEFAULTS)))
+  ifneq (,$(strip $(CORTEX_A15_TYPE)))
+    LOCAL_CORTEX_FLAGS := $(USE_GCC_DEFAULTS)
+  endif
+
+  ifneq (,$(strip $(CORTEX_A57_TYPE)))
+
+    #TODO arm64 is still experimental in sabermod, so there is no USE_GCC_DEFAULTS for it as of now.
+    #TODO Lots of testing!
+    LOCAL_CORTEX_FLAGS := -march=armv8-a -mtune=cortex-a57
+  endif
+endif
 	
-ifneq ($(strip $(ENABLE_GCC_DEFAULTS)),true)
-  ifneq (1,$(words $(filter $(LOCAL_DISABLE_TUNE), $(LOCAL_MODULE))))
-   ifneq ($(strip $(LOCAL_IS_HOST_MODULE)),true)
-    ifneq (,$(filter $(CORTEX_A15_TYPE),$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_TUNE), $(LOCAL_MODULE))))
+  ifneq ($(strip $(LOCAL_IS_HOST_MODULE)),true)
+    ifndef LOCAL_CORTEX_FLAGS
+      ifneq (,$(filter $(CORTEX_A15_TYPE),$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+        ifdef LOCAL_CFLAGS
+          LOCAL_CFLAGS += -mcpu=cortex-a15
+        else
+          LOCAL_CFLAGS := -mcpu=cortex-a15
+        endif
+      endif
+
+      ifneq (,$(filter $(CORTEX_A57_TYPE),$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+        ifdef LOCAL_CFLAGS
+          LOCAL_CFLAGS += -mcpu=cortex-a57
+        else
+          LOCAL_CFLAGS := -mcpu=cortex-a57
+        endif
+      endif
+    else
       ifdef LOCAL_CFLAGS
-        LOCAL_CFLAGS += -mcpu=cortex-a15 \
-  	    -mtune=cortex-a15 
+        LOCAL_CFLAGS += $(LOCAL_CORTEX_FLAGS)
       else
-        LOCAL_CFLAGS := -mcpu=cortex-a15 \
-	    -mtune=cortex-a15
+        LOCAL_CFLAGS := $(LOCAL_CORTEX_FLAGS)
       endif
     endif
 
     ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
       ifdef LOCAL_CFLAGS
         LOCAL_CFLAGS += -mcpu=cortex-a9 \
-	    -mtune=cortex-a9
+          -mtune=cortex-a9
       else
         LOCAL_CFLAGS := -mcpu=cortex-a9 \
-	    -mtune=cortex-a9
+          -mtune=cortex-a9
+      endif
     endif
-  endif
 
     ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
       ifdef LOCAL_CFLAGS
         LOCAL_CFLAGS += -mcpu=cortex-a8 \
-	    -mtune=cortex-a8
+          -mtune=cortex-a8
       else
         LOCAL_CFLAGS := -mcpu=cortex-a8 \
-	    -mtune=cortex-a8
+          -mtune=cortex-a8
       endif
     endif
 
     ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
       ifdef LOCAL_CFLAGS
         LOCAL_CFLAGS += -mcpu=cortex-a7 \
-	    -mtune=cortex-a7
+          -mtune=cortex-a7
       else
-      LOCAL_CFLAGS := -mcpu=cortex-a7 \
-	    -mtune=cortex-a7
+        LOCAL_CFLAGS := -mcpu=cortex-a7 \
+          -mtune=cortex-a7
       endif
     endif
-      
-    ifneq (,$(filter $(CORTEX_A57_TYPE),$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-      ifdef LOCAL_CFLAGS
-        LOCAL_CFLAGS += -mcpu=cortex-a57 \
-	    -mtune=cortex-a57
-      else
-      LOCAL_CFLAGS := -mcpu=cortex-a57 \
-	    -mtune=cortex-a57
-      endif
-    endif
-   endif
   endif
 endif
 #####
