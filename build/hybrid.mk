0fa51fd65f7b1305cbabdd8eced2d21241a7a539
Paul Beeler <pbeeler80@gmail.com>
2015-09-16 01:01:51 -0400

    [hybrid/arm] Simplify
    - GCC_4_8_MODULES was previously added for testing purposes.
      It's not being used, so remove it and all related code dependencies.
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- build/hybrid.mk
+++ build/hybrid.mk
@@ -15,25 +15,14 @@
 # Hybrid toolchain mode default to on.
 ifeq ($(strip $(TARGET_ARCH)),arm)
   ifeq (,$(filter true,$(LOCAL_IS_HOST_MODULE) $(LOCAL_CLANG)))
-    ifneq ($(filter 4.9% 5.% 6.%,$(SM_AND_NAME)),)
-      ifeq ($(filter 5.% 6.%,$(SM_AND_NAME)),)
-        ifeq (1,$(words $(filter $(GCC_4_8_MODULES), $(LOCAL_MODULE))))
-          LOCAL_CC := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/bin/arm-linux-androideabi-gcc$(HOST_EXECUTABLE_SUFFIX)
-          LOCAL_CXX := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/bin/arm-linux-androideabi-g++
-        endif
+    ifneq ($(filter 5.% 6.%,$(SM_AND_NAME)),)
+      ifeq (1,$(words $(filter $(GCC_4_9_MODULES), $(LOCAL_MODULE))))
+        LOCAL_CC := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-gcc$(HOST_EXECUTABLE_SUFFIX)
+        LOCAL_CXX := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-g++
       else
-        ifeq (1,$(words $(filter $(GCC_4_8_MODULES), $(LOCAL_MODULE))))
-          LOCAL_CC := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/bin/arm-linux-androideabi-gcc$(HOST_EXECUTABLE_SUFFIX)
-          LOCAL_CXX := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/bin/arm-linux-androideabi-g++
-        endif
-        ifeq (1,$(words $(filter $(GCC_4_9_MODULES), $(LOCAL_MODULE))))
-          LOCAL_CC := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-gcc$(HOST_EXECUTABLE_SUFFIX)
-          LOCAL_CXX := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-g++
-        else
-          ifeq (true,$(strip $(GRAPHITE_IS_ENABLED)))
-            LOCAL_CFLAGS += -floop-unroll-and-jam
-            LOCAL_LDFLAGS += -floop-unroll-and-jam
-          endif
+        ifeq (true,$(strip $(GRAPHITE_IS_ENABLED)))
+          LOCAL_CFLAGS += -floop-unroll-and-jam
+          LOCAL_LDFLAGS += -floop-unroll-and-jam
         endif
       endif
     endif
