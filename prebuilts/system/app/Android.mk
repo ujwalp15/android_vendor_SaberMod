7815481ba4be7d5f94a904188e5d769bbaa5c0fc
Joe Maples <frap129@gmail.com>
2015-09-22 18:45:16 -0400

    SaberFox: Update to latest version
    Signed-off-by: Joe Maples <frap129@gmail.com>

--- prebuilts/system/app/Android.mk
+++ prebuilts/system/app/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := FireFox-arm-SM4.9
+LOCAL_MODULE := SaberFox
 LOCAL_OVERRIDES_PACKAGES := Browser
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_SUFFIX := .apk
